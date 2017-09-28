823bb5ac77c856e3a4d42b707dace774784d07de
Бойко Алексей Сергеевич <boikoas@list.ru>
2017-09-28 19:28:01 +0300

    Добавил страницы

---
+++ Farfaded/Controllers/DetailsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Farfaded.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
